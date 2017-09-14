65569e64017b76f21beb88db3c1c8a675a1eef43
Olha Salii <osali@softserveinc.com>
2017-09-14 18:40:42 +0300

    Initial Commit

---
+++ labParallel/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  labParallel
+//
+//  Created by Olha Salii on 14.09.17.
+//  Copyright © 2017 Olha Salii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
